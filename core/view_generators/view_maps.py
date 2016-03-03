1b0997c9426a87793eb66436cfbab79ae51d3c10
Alexander Tanski <alexander.tanski@yougov.de>
2016-03-03 11:31:45 +0100

    removing the deepcopy of the view.

--- core/view_generators/view_maps.py
+++ core/view_generators/view_maps.py
@@ -277,8 +277,8 @@ class QuantipyViews(ViewMapper):
 
                 views = []
                 for rel in rel_to:
-                    new_view = copy.copy(view)
-                    new_q = copy.copy(q)
+                    new_view = View(link, name, kwargs)
+                    new_q = q._copy()
                     new_view._kwargs['rel_to'] = rel
                     views.append((new_view, new_q))
                 old_rel_to = kwargs['rel_to']
